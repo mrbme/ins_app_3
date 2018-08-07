33643ab5933390d73a13a7bfa9d11ae54562d156
Blake Epstein <blake@brightforestmedia.com>
2018-08-07 14:54:10 -0500

    updates

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,10 @@ class User < ApplicationRecord
   validates :zip_code, zipcode: { country_code: :us }
   validates :gender, presence: true
   validates :tobacco, presence: true
-  validates :license, presence: true
-  validates :gender, presence: true
-  validates :height, presence: true
-  validates :weight, presence: true
-  validates :medical, presence: true
+  # validates :license, presence: true
+  # validates :height, presence: true
+  # validates :weight, presence: true
+  # validates :medical, presence: true
   validates :birthday, presence: true
-  validates :family_illness, presence: true
+  # validates :family_illness, presence: true
 end
